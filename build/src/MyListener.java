8682964addb82b304e3bff8cb66cbd605e5415b7
Elena Milan Lopez <emilanlopez@bellarmine.edu>
2019-10-24 22:51:39 -0400

    Updated

--- build/src/MyListener.java
+++ build/src/MyListener.java
@@ -11,168 +11,253 @@
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
+import java.util.HashMap;
 
 /**
- * This class provides an empty implementation of {@link tinybasicListener},
+ * This class provides an empty implementation of {@link KnightCodeListener},
  * which can be extended to create a listener which only needs to handle a subset
  * of the available methods.
  */
-public class MyListener extends tinybasicBaseListener {
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterProgram(tinybasicParser.ProgramContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitProgram(tinybasicParser.ProgramContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterLine(tinybasicParser.LineContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitLine(tinybasicParser.LineContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterStatement(tinybasicParser.StatementContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitStatement(tinybasicParser.StatementContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterExprlist(tinybasicParser.ExprlistContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitExprlist(tinybasicParser.ExprlistContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterVarlist(tinybasicParser.VarlistContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitVarlist(tinybasicParser.VarlistContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterExpression(tinybasicParser.ExpressionContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitExpression(tinybasicParser.ExpressionContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterTerm(tinybasicParser.TermContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitTerm(tinybasicParser.TermContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterFactor(tinybasicParser.FactorContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitFactor(tinybasicParser.FactorContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterVara(tinybasicParser.VaraContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitVara(tinybasicParser.VaraContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterNumber(tinybasicParser.NumberContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitNumber(tinybasicParser.NumberContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterRelop(tinybasicParser.RelopContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitRelop(tinybasicParser.RelopContext ctx) { }
+public class MyListener extends KnightCodeBaseListener {
+
+	HashMap<String, Integer> variableMap = new HashMap();
+
+	 /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+	@Override public void enterFile(KnightCodeParser.FileContext ctx) { }
 
 	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+ 
+        @Override public void exitFile(KnightCodeParser.FileContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void enterDeclare(KnightCodeParser.DeclareContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void exitDeclare(KnightCodeParser.DeclareContext ctx) { }
+ 
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+ 
+        @Override public void enterVariable(KnightCodeParser.VariableContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void exitVariable(KnightCodeParser.VariableContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+	 @Override public void enterType(KnightCodeParser.TypeContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+	@Override public void exitType(KnightCodeParser.TypeContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void enterBody(KnightCodeParser.BodyContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void exitBody(KnightCodeParser.BodyContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void enterStat(KnightCodeParser.StatContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+	@Override public void exitStat(KnightCodeParser.StatContext ctx) { }
+
+	/**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void enterSetvar(KnightCodeParser.SetvarContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void exitSetvar(KnightCodeParser.SetvarContext ctx) { }
+ /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void enterExpr(KnightCodeParser.ExprContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void exitExpr(KnightCodeParser.ExprContext ctx) { }
+
+	/**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void enterComp(KnightCodeParser.CompContext ctx) { }
+
+	/**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void exitComp(KnightCodeParser.CompContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+	@Override public void enterPrint(KnightCodeParser.PrintContext ctx) { }
+
+	/**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void exitPrint(KnightCodeParser.PrintContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void enterRead(KnightCodeParser.ReadContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void exitRead(KnightCodeParser.ReadContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void enterDecision(KnightCodeParser.DecisionContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+ 	@Override public void exitDecision(KnightCodeParser.DecisionContext ctx) { }
+
+	/**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void enterLoop(KnightCodeParser.LoopContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void exitLoop(KnightCodeParser.LoopContext ctx) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
 	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+
 	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+	 @Override public void exitEveryRule(ParserRuleContext ctx) { }
+
 	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void visitTerminal(TerminalNode node) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void visitErrorNode(ErrorNode node) { }
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void visitTerminal(TerminalNode node) { }
+
+        /**
+         * {@inheritDoc}
+         *
+         * <p>The default implementation does nothing.</p>
+         */
+
+        @Override public void visitErrorNode(ErrorNode node) { }
 }
