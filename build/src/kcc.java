eb3879a133a95710d3dc23e240b31d4b53d27313
CalebProffitt <calebproffitt97@gmail.com>
2019-10-27 20:01:36 -0400

    Update kcc.java

--- build/src/kcc.java
+++ build/src/kcc.java
@@ -1,37 +1,52 @@
+/** This class is the compiler which uses the other files to convert a knightcode program to a java program
+* @author Ryan Meyer
+* @author Elena Milan Lopez
+* @author Caleb Proffitt
+* @version 1.0.0
+* Programming Project Two
+* CS 322 - Compiler Construction
+* Fall 2019
+*/
+
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
-public class kcc  {
-    public static void main(String[] args) throws Exception {
-        
-	String inputFile = null; 
-        if ( args.length>0 ) 
-		inputFile = args[0];
-        
-	InputStream is = System.in;
-        
-	if ( inputFile!=null ) 
-		is = new FileInputStream(inputFile);
-        
-	ANTLRInputStream input = new ANTLRInputStream(is); 
-        KnightCodeLexer lexer = new KnightCodeLexer(input); 
-        CommonTokenStream tokens = new CommonTokenStream(lexer); 
-        KnightCodeParser parser = new KnightCodeParser(tokens); 
-        
-	parser.removeErrorListeners();
+public class kcc {
 
-	parser.addErrorListener(new ErrorListener());
-
-	ParseTree tree = parser.program(); // start at the program label
-
-	ParseTreeWalker walker = new ParseTreeWalker();
-		
-	MyListener l = new MyListener();
-	walker.walk(l, tree);
-	
+        public static void main(String args[]) throws Exception {
 
 
-	}//end main
-}//end class
+                String inputFile = null;
+                if (args[1].length>0)
+                        inputFile = args[1];    //Create a new empty string. If arguments have been input in the command line, take args[1] and set that as the name of the inputFile
+
+                InputStream is = System.in;
+
+                if (inputFile !=null)
+                        is = new FileInputStream(inputFile);    //If the inputFile exists, create a new FileInputStream to read the raw data of the inputFile
+
+                ANTLRInputStream input = new ANTLRInputStream(is);
+                KnightCodeLexer lexer = new KnightCodeLexer(input);
+                CommonTokensStream tokens = new CommonTokensStream(lexer);
+                KnightCodeParser parser = new KnightCodeLexer(tokens);
+
+                parser.removeErrorListeners();
+                parser.addErrorListener(new ErrorListener());   //Use the error listener we created instead of any defaults
+
+                ParseTree tree = parser.program();
+                ParseTreeWalker walkerTexasRanger = new ParseTreeWalker();      //Create a new parse tree and a walker to walk the tree
+
+                MyListener l = new MyListener();
+                walkerTexasRanger.walk(l, tree);        //Use the listener we created to walk the parse tree
+
+                if (args[0]="source") {
+                        Runtime rt Runtime.getRuntime();
+                        Process pr = rt.exec("java kcc " + args[3]);    //If the user wants source code, run java kcc source fileName outputName
+                } else {
+                        System.out.println("Coming Soon");      //If the user wants byte code, politely refuse
+                }
+        }
+}
+
