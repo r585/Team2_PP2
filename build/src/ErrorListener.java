50d4546e43675728c92454932d003828171613a6
Ryan Meyer <ryanmeyer9195@gmail.com>
2019-10-27 22:34:09 -0400

    Commented Files

--- build/src/ErrorListener.java
+++ build/src/ErrorListener.java
@@ -1,4 +1,4 @@
-/** This class will extend the BaseErrorListener class from ANTLR, which will tell that parser what types of errors to catch.
+/** This class will extend the BaseErrorListener class from ANTLR, which will catch if an error occurs during the compiler execution.
 * @author Ryan Meyer
 * @author Caleb Proffitt
 * @author Elena Milan Lopez
@@ -9,20 +9,19 @@
 **/
 
 import org.antlr.v4.runtime.*;
-//import org.antlr.v4.runtime.misc.Nullable;
 
 import java.util.*;
 
 public class ErrorListener extends BaseErrorListener{
 
 	public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e){
-
-		List<String> stack = ((Parser) recognizer).getRuleInvocationStack();
+												
+		List<String> stack = ((Parser) recognizer).getRuleInvocationStack();   //This stack is created to store the recognized rules onto the stack
 		Collections.reverse(stack);
-		System.err.println("rule stack "+stack);
-		System.err.println("line "+line+":"+charPositionInLine+" at" + offendingSymbol + ": "+msg);
+		System.err.println("rule stack "+stack); //If an error is found, this will print the rule from the stack where it is found
+		System.err.println("line "+line+":"+charPositionInLine+" at" + offendingSymbol + ": "+msg); //If an error is found, this will print the line and char position
 		System.err.println(msg);
-		System.err.println("An error occurred during the compilation process. Please review your code.");
+		System.err.println("An error occurred during the compilation process. Please review your code."); //This generic message will print if an error has occurred 
 		System.exit(1);
 	}
 
